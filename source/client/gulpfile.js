c8f4d5703f743ff44e2e2e526b9c6ec853a4aca3
Algefmarc Anthony L. Almocera <algefalmocera@gmail.com>
2016-08-08 20:55:48 +0800

    Fix indentions

--- source/client/gulpfile.js
+++ source/client/gulpfile.js
@@ -2,31 +2,44 @@
 
 var gulp = require('gulp');
 var clean = require('gulp-clean');
+var copy = require('gulp-copy');
 
 /* TASKS
 =====================================*/
 
 gulp.task('clean', function(){
 
-  var files = [
-    // Asset folder
-    './build/css/*.css',
-    './build/js/*.js',
-    './build/images/*.jpg',
-    './build/images/*.jpeg',
-    './build/images/*.png',
-    './build/images/*.gif',
+    var files = [
+        // Asset folder
+        './build/css/*.css',
+        './build/js/*.js',
+        './build/images/*.jpg',
+        './build/images/*.jpeg',
+        './build/images/*.png',
+        './build/images/*.gif',
 
-    // Vendor folder
-    './build/vendors/css/*.css',
-    './build/vendors/js/*.js',
-    './build/app.js',
-    './build/index.html'
-  ];
+        // Vendor folder
+        './build/vendors/css/*.css',
+        './build/vendors/js/*.js',
+        './build/app.js',
+        './build/index.html'
+    ];
 
-  return gulp.src(files).pipe(clean());
+    return gulp.src(files).pipe(clean());
 
 });
+
+gulp.task('copy-files', function(){ 
+
+    var files = [
+
+    ];
+});
+
+gulp.task('build', ['clean', 'copy-files'], function(){
+});
+
+
 
 gulp.task('default', function() {
 
