8daf3bf09cb393a21fd82cee6e0699bd5567ea65
Algefmarc Anthony L. Almocera <algefalmocera@gmail.com>
2016-08-08 21:20:01 +0800

    Add sample scripts for concatenating dev/js/ scripts

---
+++ source/client/dev/js/example-script-03.js
@@ -0,0 +1,6 @@
+(function() {
+
+    'use strict';
+    console.log('Example script 03');
+
+})();
--- source/client/gulpfile.js
+++ source/client/gulpfile.js
@@ -1,8 +1,12 @@
 'use strict';
 
+/* DEPENDENCIES
+=====================================*/
+
 var gulp = require('gulp');
 var clean = require('gulp-clean');
 var copy = require('gulp-copy');
+var concat = require('gulp-concat');
 
 /* TASKS
 =====================================*/
@@ -11,18 +15,18 @@ gulp.task('clean', function(){
 
     var files = [
         // Asset folder
-        './build/css/*.css',
-        './build/js/*.js',
-        './build/images/*.jpg',
-        './build/images/*.jpeg',
-        './build/images/*.png',
-        './build/images/*.gif',
+        'build/css/*.css',
+        'build/js/*.js',
+        'build/images/*.jpg',
+        'build/images/*.jpeg',
+        'build/images/*.png',
+        'build/images/*.gif',
 
         // Vendor folder
-        './build/vendors/css/*.css',
-        './build/vendors/js/*.js',
-        './build/app.js',
-        './build/index.html'
+        'build/vendors/css/*.css',
+        'build/vendors/js/*.js',
+        'build/app.js',
+        'build/index.html'
     ];
 
     return gulp.src(files).pipe(clean());
@@ -46,7 +50,23 @@ gulp.task('copy-files', ['clean'], function(){
         .pipe(copy('./build', options));
 });
 
-gulp.task('build', ['copy-files'], function(){
+gulp.task('concat-js', function(){ 
+
+    var files = [
+        'dev/js/example-script-01.js',
+        'dev/js/example-script-02.js',
+        'dev/js/example-script-03.js',
+    ];
+
+    var destination = 'build/js/'
+
+    return gulp.src(files)
+        .pipe(concat('all.js'))
+        .pipe(gulp.dest(destination));
+
+});
+
+gulp.task('build', ['copy-files', 'concat-js'], function(){
 });
 
 
