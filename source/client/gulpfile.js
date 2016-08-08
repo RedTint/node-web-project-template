88f44d16dd7caa698da2f255e9459499387c540f
Algefmarc Anthony L. Almocera <algefalmocera@gmail.com>
2016-08-08 20:41:15 +0800

    Move folders outside of /assets; move app.js and index.html to dev

--- source/client/gulpfile.js
+++ source/client/gulpfile.js
@@ -6,8 +6,26 @@ var clean = require('gulp-clean');
 /* TASKS
 =====================================*/
 
-gulp.task('config-clean', function(){
-  gulp.src('client/build/assets/css/*.css').pipe(clean());
+gulp.task('clean', function(){
+
+  var files = [
+    // Asset folder
+    './build/css/*.css',
+    './build/js/*.js',
+    './build/images/*.jpg',
+    './build/images/*.jpeg',
+    './build/images/*.png',
+    './build/images/*.gif',
+
+    // Vendor folder
+    './build/vendors/css/*.css',
+    './build/vendors/js/*.js',
+    './build/app.js',
+    './build/index.html'
+  ];
+
+  return gulp.src(files).pipe(clean());
+
 });
 
 gulp.task('default', function() {
