ccbd011de4c9ede8703baf27cc48c55c913378bd
Algefmarc Anthony L. Almocera <algefalmocera@gmail.com>
2016-08-11 14:39:20 +0800

    Separation of concerns, fix client-route

--- source/server/routes/client-route.js
+++ source/server/routes/client-route.js
@@ -1,5 +1,20 @@
 'use strict';
 
+/* REQUIRE DEPENDENCIES
+================================================*/
+var express = require('express');
+var path = require('path');
+
+/* CONFIG
+================================================*/
+var config = require('./configs/config.js');
+var clientPath = config.client.path;
+var clientAbsPath = __dirname + '/' + clientPath;
+
+/* 
+    app - express instance
+        - should be passed to make sure we are using only a single instance of 'express'
+*/
 module.exports = function(app) {
     
     app.use(express.static(clientPath));
