acc0b7b1800afdc9cf3e0d313bcfe3bd5333040f
Algefmarc Anthony L. Almocera <algefalmocera@gmail.com>
2016-08-12 23:25:16 +0800

    Re-order sections to have better feel for the code. It makes sense to have Load Routes just before Register Routes section

--- source/server/app.js
+++ source/server/app.js
@@ -5,11 +5,6 @@
 var express         = require('express');
 var app             = express();
 
-/* LOAD ROUTES
-================================================*/
-var clientRoute     = require('./routes/client-route');
-var apiRoutes       = require('./api/routes/routes');
-
 /* CONFIG
 ================================================*/
 
@@ -18,8 +13,18 @@ var clientPath = config.client.path;
 var clientAbsPath = __dirname + '/' + clientPath;
 var ports = config.ports;
 
-/* REGISTER ROUTES
+/* LOAD ROUTES
 ================================================*/
+var clientRoute     = require('./routes/client-route');
+var apiRoutes       = require('./api/routes/routes');
+
+/* REGISTER ROUTES
+   - It would be great to have all routings
+     called in this section
+   - express instance 'app' is passed as
+     parameter to all route declarations
+================================================*/
+console.log('# Registering routes');
 clientRoute(app);
 apiRoutes(app);
 
@@ -31,7 +36,7 @@ console.log('# Running server in \'' + config.description + '\'');
 // Add SSL Certificate Here
 // Run listen to https and http here.
 
-console.log('Client Path: ' + clientPath);
+console.log('# Client Path: ' + clientPath);
 
 /* LAUNCH SERVER
 ================================================*/
