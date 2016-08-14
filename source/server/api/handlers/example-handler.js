a3c1b832d0e54c4581b477aa18f6d2531b45fa71
Algefmarc Anthony L. Almocera <algefalmocera@gmail.com>
2016-08-14 11:46:40 +0800

    Refactor example-handler Add an example for getting book by id Add a bit of error handling

--- source/server/api/handlers/example-handler.js
+++ source/server/api/handlers/example-handler.js
@@ -1,16 +1,48 @@
+/*
+    EXAMPLE HANDLER
+    - This is an example of the recommened way the API routes should be defined.
+    - Regarding the pattern (e.g: repository pattern) you want to use for getting your data, it is all up
+    to you. I have tried to have this loosely-coupled as possible.
+*/
+
 'use script';
 
 module.exports = function(app) {
 
-    console.log('# injecting /api/geteProjects');
-    app.get('/api/getProjects', function(req, res) {
+    // dummy data
+    var data = {
+        "books": [
+            { "title": "Harry Potter", "author": "J.K. Rowling" },
+            { "title": "Lord of the Rings", "author": "J. R. R. Tolkien" }
+        ]
+    };
 
-        res.send({
-            "books": [
-                { "title": "Harry Potter", "author": "J.K. Rowling" }
-            ]
-        });
+    // Return list of books
+    app.get('/api/example/getBooks', function(req, res) {
+
+        res.send(data.books);
+
+    });
+
+    // Return book by ID
+    app.post('/api/example/getBooks', function(req, res) {
+
+        // Preferred place for getting data
+        var id = req.body.id;
+
+        /* Data Validation
+           - It's generally up to you on how you validate your data 
+             this is just an example
+        */
+        if (id === undefined || id === null || isNaN(id)) {
+            res.send(500, { status: 500, message: 'invalid parameter(s)' });
+        }
+
+        // return data with index 'id'
+        res.send(data.books[id]);
 
     });
 
 }
+
+
